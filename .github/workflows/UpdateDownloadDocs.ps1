5f155822ed3890c3d1ec4f6b4d120ff9212e5198
ChristianFloecklbauer <53088011+ChristianFloecklbauer@users.noreply.github.com>
2024-09-23 20:44:55 +0200

    Test

---
+++ .github/workflows/UpdateDownloadDocs.ps1
@@ -0,0 +1,60 @@
+﻿$TenantId = "a73cb8c1-7950-4ec2-b413-7edf11627a80"
+$ClientId = $env:CLIENT_ID
+$ClientSecret = $env:CLIENT_SECRET
+$Scope = "https://storage.azure.com/.default"
+$TokenEndpoint = "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token"
+$Body = @{
+    client_id     = $ClientId
+    scope         = $Scope
+    grant_type    = "client_credentials"
+    client_secret = $ClientSecret
+}
+$Response = Invoke-RestMethod -Method Post -Uri $TokenEndpoint -ContentType "application/x-www-form-urlencoded" -Body $Body
+
+$AccessToken = $Response.access_token
+$StorageAccountName = "belwareapps"
+$ContainerUrl = "https://belwareapps.table.core.windows.net/MaintainedBCVersions"
+$Headers = @{
+    Authorization = "Bearer $accessToken"
+    "x-ms-version" = "2020-04-08"
+    "Accept" = "application/json;odata=nometadata"
+}
+$Response = Invoke-RestMethod -Uri $ContainerUrl -Method Get -Headers $Headers
+
+$BcVersionList = $Response.value | ConvertTo-Json
+$BcVersions = @()
+foreach ($BcVersion in $BcVersionList){
+    $BcVersions += $BcVersion.PartitionKey
+}
+$SortedVersions = $BcVersions | ForEach-Object { [System.Version]$_ } | Sort-Object -Descending
+
+foreach ($Version in $sortedVersions){
+    $DownloadPageContent += "#### $Version`n"
+    $TableName = "AppVersions"
+    $StorageTable = Get-AzStorageTable -Name $tableName -Context $StorageContext
+    $CloudTable = $storageTable.CloudTable
+    $TableFilter = "(RowKey eq '${Version}')"
+    $AppVersions = Get-AzTableRow -Table $CloudTable -CustomFilter $TableFilter
+    foreach($AppVersion in $AppVersions) {
+        $FormattedAppName = $($AppVersion.PartitionKey.Replace(' ','%20'))
+        $BcVersionNumber = $($AppVersion.RowKey)
+        $DownloadPageContent += "[$($AppVersion.PartitionKey)](https://belwareapps.blob.core.windows.net/apps/$BcVersionNumber/$FormattedAppName/${BcVersionNumber}_${FormattedAppName}_$($AppVersion.AppVersion)_RT.app)`n"
+    }
+}
+
+$Base64DownloadPageContent = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($DownloadPageContent))
+foreach ($WebsiteLanguage in $WebsiteLanguages){
+    $ResponseMessage = ConvertFrom-Json(curl -Uri "https://api.github.com/repos/ChristianFloecklbauer/New-Docs-Test/contents/content/${WebsiteLanguage}-${WebsiteLanguage}/Apps/Downloads/_index.md" -Method Get -H @{"Authorization"= "Bearer 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"})
+    $RequestBody = @"
+    {
+      "message": "Updated Download Page ${WebsiteLanguage}",
+      "committer": {
+        "name": "Power Automate Flow",
+        "email": "admin@belware.de"
+      },
+        "content": "${Base64DownloadPageContent}",
+        "sha": "$($ResponseMessage.sha)"
+    }
+"@
+    curl -Uri "https://api.github.com/repos/ChristianFloecklbauer/New-Docs-Test/contents/content/${WebsiteLanguage}-${WebsiteLanguage}/Apps/Downloads/_index.md" -Method Put -H @{"Authorization"= "Bearer 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"} -Body $RequestBody
+}
